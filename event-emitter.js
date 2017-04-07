21e41416543fb29e2c57cce832ae94518585a44e
Viacheslav Morozyik <slava@3cn8jc2.ncf.wdc.com>
2017-04-07 17:53:46 +0300

    a path to event-emiter is fixed

--- event-emitter.js
+++ event-emitter.js
@@ -1,2 +1,2 @@
-import EventEmitter from '../react-native/Libraries/EventEmitter/EventEmitter';
+import EventEmitter from '../../node_modules/react-native/Libraries/EventEmitter/EventEmitter';
 export default EventEmitter;
